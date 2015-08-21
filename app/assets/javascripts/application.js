058f8ffffd390bfb79c21366ee50ee8424c07e4b
Ben Wheat <BWheatie@gmail.com>
2015-08-21 10:35:30 -0500

    Changed title text

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,13 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require new-pin
+
+
+insertImage = (data) ->
+  $('#image-list').append("<div class=\"panel panel-default\">").append("<div class=\"panel-heading\"><img src=\""+data.url+"\" width=\"250\"></div><div class\"panel-body\">"+data.notes+"</div>")
+
+$(document).ready ->
+  $("#new_pin").on "ajax:success", (e, data, status, xhr) ->
+    console.log("new pin success")
+    insertImage(data)
+    $("#new-pin-modal").modal("hide")
--- app/assets/javascripts/new-pin.coffee
+++
@@ -1,8 +0,0 @@
-insertImage = (data) ->
-  $('#image-list').append("<div class=\"panel panel-default\">").append("<div class=\"panel-heading\"><img src=\""+data.url+"\" width=\"250\"></div><div class\"panel-body\">"+data.notes+"</div>")
-
-$(document).ready ->
-  $("#new_pin").on "ajax:success", (e, data, status, xhr) ->
-    console.log("new pin success")
-    insertImage(data)
-    $("#new-pin-modal").modal("hide")
